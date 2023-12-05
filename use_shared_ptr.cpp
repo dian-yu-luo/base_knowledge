e640b162d4e04025a80e1c78ccfed9ad04087d35
dianyuluo <1739556970@qq.com>
2023-12-05 20:55:25 +0000

    使用样例

--- use_shared_ptr.cpp
+++ use_shared_ptr.cpp
@@ -10,12 +10,31 @@ void foo(std::shared_ptr<int> i)
 }
 int main(int argc, char const *argv[])
 {
-    // 功能是在动态内存中分配一个对象并初始化它，返回指向此对象的shared_ptr
+    std::cout << "\033[31m"
+              << "begining--------------------"
+              << "\033[0m" << std::endl;
     auto pointer = std::make_shared<int>(10);
-    foo(pointer);
-    // 我可以想用指针一样的用它
-    std::cout << *pointer << std::endl; // 11
-
-    
+    auto pointer2 = pointer; // 引用计数 +1
+    auto pointer3 = pointer; // 引用计数 +1
+                             // 这样不会增加引用计数
+    std::cout << "pointer.use_count() = " << pointer.use_count() << std::endl;
+    // 3
+    std::cout << "pointer2.use_count() = " << pointer2.use_count() << std::endl; // 3
+    std::cout << "pointer3.use_count() = " << pointer3.use_count() << std::endl; // 3
+    pointer2.reset();
+    std::cout << "reset pointer2:" << std::endl;
+    std::cout << "pointer.use_count() = " << pointer.use_count() << std::endl;
+    // 2
+    std::cout << "pointer2.use_count() = "
+              << pointer2.use_count() << std::endl;
+    // pointer2 已 reset; 0
+    std::cout << "pointer3.use_count() = " << pointer3.use_count() << std::endl; // 2
+    pointer3.reset();
+    std::cout << "reset pointer3:" << std::endl;
+    std::cout << "pointer.use_count() = " << pointer.use_count() << std::endl;
+    // 1
+    std::cout << "pointer2.use_count() = " << pointer2.use_count() << std::endl; // 0
+    std::cout << "pointer3.use_count() = "
+              << pointer3.use_count() << std::endl;
     return 0;
 }
