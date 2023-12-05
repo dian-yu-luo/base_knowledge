e9905be3eed84bebd7438fd434fc0f8cdf36592c
dianyuluo <1739556970@qq.com>
2023-12-05 21:08:28 +0000

    使用shared_ptr的理念,

--- use_shared_ptr.cpp
+++ use_shared_ptr.cpp
@@ -1,7 +1,7 @@
 #include <memory>
 #include <iostream>
 using namespace std;
-
+// TODO 这个东西的实现原理是什么?
 // 这里的一个函数,参数是 shared_ptr<int> i   //咱也看不懂,这叫个啥,但是
 // 这个智能指针指向的是一个int 类型的值
 void foo(std::shared_ptr<int> i)
@@ -17,6 +17,15 @@ int main(int argc, char const *argv[])
     auto pointer2 = pointer; // 引用计数 +1
     auto pointer3 = pointer; // 引用计数 +1
                              // 这样不会增加引用计数
+    {
+        // 作用域里面的值都在指着,但是作用域没事了之后,外面还在处理这个东西,
+        // 简单的说呢就是虽然作用域完事了,但是外面的作用域依旧能够管理里面的内容
+        auto pointer_dian = std::make_shared<int>(99);
+        pointer2 = pointer_dian; // 引用计数 +1
+        pointer3 = pointer_dian;
+    }
+
+    std::cout << "pointer2 value is :" << *pointer2 << std::endl;
     std::cout << "pointer.use_count() = " << pointer.use_count() << std::endl;
     // 3
     std::cout << "pointer2.use_count() = " << pointer2.use_count() << std::endl; // 3
