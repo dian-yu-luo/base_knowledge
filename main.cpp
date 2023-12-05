b4bbd01469c25b82e6b21f82fec701826ab76a96
dianyuluo <1739556970@qq.com>
2023-12-05 20:29:37 +0000

    不采用智能指针的方法

--- main.cpp
+++ main.cpp
@@ -8,9 +8,46 @@ void tesManualManageMemery()
     int *ptr = new int; // 动态分配一个int类型的内存
     *ptr = 10;
 }
+// gpt 生成的方案,尝试使用
+class MemoryRAII
+{
+public:
+    // 构造函数,数据初始化表,
+    // data 是一个指针,所以也能视作是一个数组开头指向的内容
+    MemoryRAII(size_t size) : data(new int[size])
+    {
+        std::cout << "Allocated memory of size: " << size << std::endl;
+    }
+    // 析构函数就是把指针指向的内容删除掉
+    ~MemoryRAII()
+    {
+        delete[] data;
+        std::cout << "Freed memory" << std::endl;
+    }
+    // 基本的修改函数方法
+    void writeData(size_t index, int value)
+    {
+        data[index] = value;
+    }
+
+private:
+    int *data;
+};
 
 int main(int argc, char const *argv[])
 {
+
+    try
+    {
+        MemoryRAII memory(10);
+        memory.writeData(1, 42);
+        memory;
+        // 在此处离开作用域时，memory对象的析构函数会自动释放内存
+    }
+    catch (const std::exception &e)
+    {
+        std::cout << "Exception: " << e.what() << std::endl;
+    }
 
     // 先不要用类来进行完成,写直接写
     // 首先,作为cpp 就不用c语言的方案{malloc 和 free 做了,虽然我不知道这俩的缺点是啥,但是
---
+++ raiitool.h
@@ -0,0 +1,18 @@
+// 这是一个不使用智能指针的raii 实现的方式
+class raiitool
+{
+private:
+    /* data */
+public:
+    raiitool(/* args */);
+
+    ~raiitool();
+};
+
+raiitool::raiitool(/* args */)
+{
+}
+
+raiitool::~raiitool()
+{
+}
