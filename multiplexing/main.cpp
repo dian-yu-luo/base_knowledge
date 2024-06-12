613ae4b15c9587e9701aa711894eef5b4743d519
dianyuluo <1739556970@qq.com>
2024-06-12 08:45:25 +0000

    重新组织文件的组建形式

---
+++ multiplexing/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <help.h>
+int main(int argc, char const *argv[])
+{
+    help h;
+    h.foo();
+    return 0;
+}
---
+++ src/help.cpp
@@ -0,0 +1,14 @@
+#include "help.h"
+#include <iostream>
+help::help()
+{
+}
+
+help::~help()
+{
+}
+
+void help::foo()
+{
+    printf("foo\n");
+}
