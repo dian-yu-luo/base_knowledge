9f33ca8b959956c440eec0ba7781faffbc6697e5
dianyuluo <1739556970@qq.com>
2024-07-10 23:42:13 +0000

    递归的锁

--- multiplexing/test_deadlock.cpp
+++ multiplexing/test_deadlock.cpp
@@ -1,27 +1,21 @@
 #include <iostream>
-#include <string>
-#include <thread>
 #include <mutex>
 
-int main() {
-    std::mutex mtx1;
-    std::mutex mtx2;
+std::mutex mtx1;
 
-    std::thread t1([&] {
-        for (int i = 0; i < 1000; i++) {
-            std::lock(mtx1, mtx2); // 实现里面会进行尝试性加锁
-            mtx1.unlock();
-            mtx2.unlock();
-        }
-    });
-    std::thread t2([&] {
-        for (int i = 0; i < 1000; i++) {
-            std::lock(mtx2, mtx1);
-            mtx2.unlock();
-            mtx1.unlock();
-        }
-    });
-    t1.join();
-    t2.join();
+void other() {
+    mtx1.lock();
+    // do something
+    mtx1.unlock();
+}
+
+void func() {
+    mtx1.lock();
+    other();
+    mtx1.unlock();
+}
+
+int main() {
+    func();
     return 0;
 }
