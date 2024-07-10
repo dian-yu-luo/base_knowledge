0d7deb9371f0fb7bf46c6cef72ad1c0fc28905ad
dianyuluo <1739556970@qq.com>
2024-07-10 23:36:20 +0000

    锁的顺序

--- multiplexing/test_deadlock.cpp
+++ multiplexing/test_deadlock.cpp
@@ -9,18 +9,19 @@ int main() {
 
     std::thread t1([&] {
         for (int i = 0; i < 1000; i++) {
+            /* 避免一种情况,同时获得两个锁,也就是说我要是尝试获得第二个锁的时候,最好放弃一个锁 */
             mtx1.lock();
+            mtx1.unlock();
             mtx2.lock();
             mtx2.unlock();
-            mtx1.unlock();
         }
     });
     std::thread t2([&] {
         for (int i = 0; i < 1000; i++) {
             mtx2.lock();
+            mtx2.unlock();
             mtx1.lock();
             mtx1.unlock();
-            mtx2.unlock();
         }
     });
     t1.join();
