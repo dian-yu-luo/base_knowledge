b1b0c9ba23a094b893c102539c664f91aec34175
dianyuluo <1739556970@qq.com>
2024-07-10 23:25:24 +0000

    lock_guard

--- multiplexing/use_mutex.cpp
+++ multiplexing/use_mutex.cpp
@@ -3,30 +3,29 @@
 #include <thread>
 #include <vector>
 #include <mutex>
-// 在这里需要加锁,锁使用方法也比较统一
-/* 
-定义锁
-锁上,临界区工作
- */
 
 int main() {
     std::vector<int> arr;
     std::mutex mtx;
     std::thread t1([&] {
         for (int i = 0; i < 1000; i++) {
-            mtx.lock();
+            /* lock_guard 的写法,直接在作用域中完成锁的任务,会自动析构的 */
+            std::lock_guard grd(mtx);
             arr.push_back(1);
-            mtx.unlock();
         }
     });
     std::thread t2([&] {
         for (int i = 0; i < 1000; i++) {
-            mtx.lock();
+            std::lock_guard grd(mtx);
             arr.push_back(2);
-            mtx.unlock();
         }
     });
     t1.join();
     t2.join();
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+        std::cout<<arr[i]<<" ";
+    }
+    
     return 0;
 }
