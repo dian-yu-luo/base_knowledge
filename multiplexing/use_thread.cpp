cb590615deaa25d0e1dfcdaedba866aa91ad1437
dianyuluo <1739556970@qq.com>
2024-07-10 23:13:11 +0000

    造了一个线程池

--- multiplexing/use_thread.cpp
+++ multiplexing/use_thread.cpp
@@ -18,19 +18,31 @@ void interact() {
     std::cout << "Hi, " << name << std::endl;
 }
 
-std::vector<std::thread> pool;
+class ThreadPool {
+    std::vector<std::thread> m_pool;
+
+public:
+    void push_back(std::thread thr) {
+        m_pool.push_back(std::move(thr));
+    }
+
+    ~ThreadPool() {                      // main 函数退出后会自动调用
+        for (auto &t: m_pool) t.join();  // 等待池里的线程全部执行完毕
+    }
+};
+
+ThreadPool tpool;
 
 void myfunc() {
     std::thread t1([&] {
         download("hello.zip");
     });
     // 移交控制权到全局的 pool 列表，以延长 t1 的生命周期
-    pool.push_back(std::move(t1));
+    tpool.push_back(std::move(t1));
 }
 
 int main() {
     myfunc();
     interact();
-    for (auto &t: pool) t.join();  // 等待池里的线程全部执行完毕
     return 0;
 }
