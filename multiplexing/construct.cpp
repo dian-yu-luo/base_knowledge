938ff6ba7ce2b741120bdb0cf9c92a4dd064e27f
dianyuluo <1739556970@qq.com>
2024-07-10 23:12:19 +0000

    拷贝什么的无所谓

--- multiplexing/construct.cpp
+++ multiplexing/construct.cpp
@@ -1,5 +1,4 @@
 
-
 #include <memory>
 #include <vector>
 #include <string>
@@ -12,24 +11,25 @@
 #include <utility>
 using namespace std;
 
-// 实现深拷贝：
+
+//实现深拷贝：
 class HasPtr
 {
 public:
-    // 默认构造函数
-    HasPtr(const std::string &s = std::string()) : ps(new std::string(s)), i(0)
+    //默认构造函数
+    HasPtr(const std::string& s = std::string()) : ps(new std::string(s)), i(0)
     {
         cout << "HasPtr() Address: " << this << "\n";
     }
 
-    // 拷贝构造函数
-    HasPtr(const HasPtr &data) : ps(new string(*(data.ps))), i(data.i)
+    //拷贝构造函数
+    HasPtr(const HasPtr& data) : ps(new string(*(data.ps))), i(data.i)
     {
         cout << "HasPtr(const HasPtr& data) Address: " << this << "\n";
     }
 
-    // 移动构造函数
-    HasPtr(HasPtr &&rhs) noexcept : ps(rhs.ps), i(rhs.i)
+    //移动构造函数
+    HasPtr(HasPtr&& rhs) noexcept : ps(rhs.ps), i(rhs.i)
     {
         rhs.ps = nullptr;
         rhs.i = 0;
@@ -39,17 +39,18 @@ public:
     // 析构函数
     ~HasPtr()
     {
+        cout <<"freeing address: "<< ps << "\n";
         delete ps;
         cout << "~HasPtr() Address: " << this << " \n";
     }
 
-    // 释放HasPtr类对象占用的资源。
-    // ps指向的对象在堆上分配，而数据成员又是原始指针，必须手动管理内存。
-    // i是一个int类型对象，它的内存在栈上分配，而不是在堆上。
-    // 当HasPtr类对象被销毁时，内置类型内存自动释放，无需（也不能）手动去delete它。
+    //释放HasPtr类对象占用的资源。
+    //ps指向的对象在堆上分配，而数据成员又是原始指针，必须手动管理内存。
+    //i是一个int类型对象，它的内存在栈上分配，而不是在堆上。
+    //当HasPtr类对象被销毁时，内置类型内存自动释放，无需（也不能）手动去delete它。
 
 private:
-    std::string *ps;
+    std::string* ps;
     int i;
 };
 
--- multiplexing/use_thread.cpp
+++ multiplexing/use_thread.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <thread>
 #include <string>
+#include <vector>
 
 void download(std::string file) {
     for (int i = 0; i < 10; i++) {
@@ -17,17 +18,19 @@ void interact() {
     std::cout << "Hi, " << name << std::endl;
 }
 
+std::vector<std::thread> pool;
+
 void myfunc() {
     std::thread t1([&] {
         download("hello.zip");
     });
-    t1.detach();
-    /* 线程和变量的关系就没了 */
-    // t1 所代表的线程被分离了，不再随 t1 对象销毁
+    // 移交控制权到全局的 pool 列表，以延长 t1 的生命周期
+    pool.push_back(std::move(t1));
 }
 
 int main() {
     myfunc();
     interact();
+    for (auto &t: pool) t.join();  // 等待池里的线程全部执行完毕
     return 0;
 }
