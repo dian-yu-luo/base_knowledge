b3cf6178863a04a0aca62b6deee36c14da5eafe7
dianyuluo <1739556970@qq.com>
2024-07-26 21:38:19 +0000

    生成一个新节点,然后返回节点的指针,

--- data_structure/skip_list.h
+++ data_structure/skip_list.h
@@ -14,6 +14,10 @@ public:
     SkipList sl=1;
      */
     explicit SkipList(Comparator cmp, Arena *arena);
+
+    // 功能函数
+    Node *NewNode(const Key &key, int height);
+
     Comparator const compare_;
     /* 这个指针永远指向这个对象 */
     Arena *const arena_; // Arena used for allocations of nodes
@@ -25,4 +29,14 @@ SkipList<Key, Comparator>::SkipList(Comparator cmp, Arena *arena)
     : compare_(cmp),
       arena_(arena)
 {
+}
+// 因为在函数里面定义的node ,所以函数需要定义作用域typename SkipList<Key, Comparator>::Node
+template <typename Key, class Comparator>
+typename SkipList<Key, Comparator>::Node *SkipList<Key, Comparator>::NewNode(const Key &key, int height)
+{
+    // *const 符号和指针地址是绑定的,
+    char *const node_memory = arena_->AllocateAligned(
+        sizeof(Node) + sizeof(std::atomic<Node *>) * (height - 1));
+    return new (node_memory) Node(key);
+    return nullptr;
 }
