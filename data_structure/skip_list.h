ee00110c32b159bca2a23fa43c58f5b0736624dd
dianyuluo <1739556970@qq.com>
2024-07-26 21:33:19 +0000

    第一个构造函数

--- data_structure/skip_list.h
+++ data_structure/skip_list.h
@@ -9,10 +9,20 @@ private:
 
 public:
     // 开始定义各种构造函数
+    // 1 禁止隐式转换,也就是 禁止下面的用法
+    /*
+    SkipList sl=1;
+     */
     explicit SkipList(Comparator cmp, Arena *arena);
+    Comparator const compare_;
+    /* 这个指针永远指向这个对象 */
+    Arena *const arena_; // Arena used for allocations of nodes
+    Node *const head_;
 };
 
 template <typename Key, class Comparator>
-inline SkipList<Key, Comparator>::SkipList(Comparator cmp, Arena *arena)
+SkipList<Key, Comparator>::SkipList(Comparator cmp, Arena *arena)
+    : compare_(cmp),
+      arena_(arena)
 {
 }
