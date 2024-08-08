5346c788a11c4c7e29cedfd9685a42a607408c36
dianyuluo <1739556970@qq.com>
2024-08-08 02:44:19 +0000

    static

--- interview/static_.cpp
+++ interview/static_.cpp
@@ -1,20 +1,23 @@
+// 只在当前文件中使用
 static int fff()
 {
 }
-
 int func()
 {
+    // 静态存储区上面
     static int a;
 }
 
 class static_
 {
 private:
+    // 只保存一个变量
     static int a;
 
 public:
     static_(/* args */);
     ~static_();
+    // 可以直接使用,但是只能访问 static 的成员变量
     static void func()
     {
     }
